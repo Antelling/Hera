0c28d46d91596e735e79e3b17e18cbe56860f2fd
Anthony Dellinger <18DellingerA@smsd.local>
2017-03-29 21:14:19 -0400

    added try catch to validate

--- validator/validate.py
+++ validator/validate.py
@@ -17,51 +17,51 @@ def val(*, people_pre, couples_raw_pre, couples_xy_pre, alg_gen, maps_post):
     }
 
     for alg in alg_gen():
-        total_score_map = {}
+        try:
+            total_score_map = {}
 
-        for i, couple in enumerate(couples_raw):
-            new_couples = copy.deepcopy(couples_raw)
-            del new_couples[i]
+            for i, couple in enumerate(couples_raw):
+                new_couples = copy.deepcopy(couples_raw)
+                del new_couples[i]
 
-            for processor in couples_raw_pre:
-                new_couples = processor.transform(new_couples)
-            couples_xy = data.make.couples_xy(new_couples, people_raw)
-            for processor in couples_xy_pre:
-                couples_xy = processor.transform(couples_xy)
+                for processor in couples_raw_pre:
+                    new_couples = processor.transform(new_couples)
+                couples_xy = data.make.couples_xy(new_couples, people_raw)
+                for processor in couples_xy_pre:
+                    couples_xy = processor.transform(couples_xy)
 
-            alg.fit(*couples_xy)
+                alg.fit(*couples_xy)
 
-            maps = {
-                "scoreable": {"one-way": {}},  # eg one-way, averaged, normalized
-                "misc": {}  # eg mono-couples, pop-list
-            }
+                maps = {
+                    "scoreable": {"one-way": {}},  # eg one-way, averaged, normalized
+                    "misc": {}  # eg mono-couples, pop-list
+                }
 
-            for person in people_raw:
-                soulmate = alg.predict_for_single_point(people_raw[person])
-                maps["scoreable"]["one-way"][person] = vector_math.get_rec(people_raw, soulmate)
+                for person in people_raw:
+                    soulmate = alg.predict_for_single_point(people_raw[person])
+                    maps["scoreable"]["one-way"][person] = vector_math.get_rec(people_raw, soulmate)
 
-            for processor in maps_post:
-                maps = processor.transform(maps)
+                for processor in maps_post:
+                    maps = processor.transform(maps)
 
-            score_map = score.score(maps, couple)
-            for m in score_map:
-                if not m in total_score_map:
-                    total_score_map[m] = []
-                total_score_map[m] += score_map[m]
+                score_map = score.score(maps, couple)
+                for m in score_map:
+                    if not m in total_score_map:
+                        total_score_map[m] = []
+                    total_score_map[m] += score_map[m]
 
-        for m in total_score_map:
-            total_score_map[m] = [np.percentile(total_score_map[m], 80),
-                                  np.mean(total_score_map[m]),
-                                  np.median(total_score_map[m])]
+            for m in total_score_map:
+                total_score_map[m] = [np.percentile(total_score_map[m], 80),
+                                      np.mean(total_score_map[m]),
+                                      np.median(total_score_map[m])]
 
-        for m in total_score_map:
-            if total_score_map[m][1] < best["score"]:
-                best["score"] = total_score_map[m][1]
-                best["alg"] = alg
-                best["post"] = m
-                best["pre"] = [people_pre, couples_raw_pre, couples_xy_pre]
-                best["people"] = people_pre
-                colors.red(best)
-                colors.green(best["score"])
+            for m in total_score_map:
+                if total_score_map[m][1] < best["score"]:
+                    best["score"] = total_score_map[m][1]
+                    best["alg"] = alg
+                    best["post"] = m
+                    best["pre"] = [people_pre, couples_raw_pre, couples_xy_pre]
+        except Exception as e:
+            colors.red(e)
 
     return best
