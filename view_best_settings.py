4039e710a5be050c565dab5d651ddac3552b0335
Anthony Dellinger <18dellingera@bubblers.us>
2018-01-09 09:41:59 -0500

    changed multi_target.py

--- view_best_settings.py
+++ view_best_settings.py
@@ -1,13 +1,24 @@
 from sklearn.externals import joblib
 import os
 import colors
+import json
+import numpy as np
 
-files = os.listdir("models")
-files.sort()
+info = json.loads(open("model_info.json").read())
 
-for file in files:
+m2f = []
+f2m = []
+
+for model in info:
+    m2f.append((model["name"], np.mean(model["m2f"])))
+    f2m.append((model["name"], np.mean(model["f2m"])))
+
+m2f.sort(key=lambda x:x[1])
+f2m.sort(key=lambda x:x[1])
+
+for i, m in enumerate(m2f):
     colors.blue("-----------------------------------------------------------------------")
-    best = joblib.load(os.path.join("models", file))
+    best = joblib.load(os.path.join("models", m[0]))
 
     params = best.get_params()
     keys = list(params.keys())
@@ -17,6 +28,7 @@ for file in files:
         print(colors.underline(key) + ": " + str(params[key]).replace("\n", ""))
 
     print("\033[92mmodel\033[0m: " + str(params["regressor"].model))
-    colors.red(file)
+    colors.red("m2f: " + str(m[1]))
+    colors.red("f2m: " + str(f2m[i][1]))
 
     input("")
