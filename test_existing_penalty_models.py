ddc66bce236c6ea970fd162fc81c04b03edfa29d
Anthony Dellinger <18dellingera@bubblers.us>
2018-01-12 13:14:20 -0500

    fixed test penalty

--- test_existing_penalty_models.py
+++ test_existing_penalty_models.py
@@ -1,10 +1,47 @@
 """see test_existing_models.py for explanation"""
 
-import validator
+import validator, data, random
 import os, json
 from sklearn.externals import joblib
 from postprocessing import Average, MetricEqualizer
 
+
+def gen_penalty(months):
+    if months > 23: return 1
+    if months > 3: return 2
+    return 3
+
+
+couples = data.get.couples_raw()
+people = data.get.people_raw()
+people_xy, names = data.get.people_xy()
+
+for i, couple in enumerate(couples):
+    penalty = 0 if couple["married"] == True else gen_penalty(couple["length"])
+    couple["penalty"] = penalty
+    couple[i] = couple
+
+
+def gen_training_data(couples):
+    X, y = [], []
+
+    for couple in couples:
+        x = people[couple["male"]]["position"] + people[couple["female"]]["position"]
+        X.append(x)
+        y.append(couple["penalty"])
+
+    # now we need some negative examples
+    for _ in range(len(couples) * 3):
+        m = random.choice(people_xy)
+        f = random.choice(people_xy)
+        vector = m + f
+        if vector not in X:
+            X.append(vector)
+            y.append(6)
+    return X, y
+
+
+X, y = gen_training_data(couples)
 
 def gen_info(model):
     m = joblib.load(os.path.join("penalty_models", model))
@@ -16,7 +53,7 @@ def gen_info(model):
     scores = []
     for _ in range(5):
         print("*", end="")
-        scores.append(validator.penalty_val([m], [Average(), MetricEqualizer(metric="percentage")], swap)["score"])
+        scores.append(validator.penalty_val(m, X, y, couples, [Average(), MetricEqualizer(metric="percentage")])["score"])
     model_info[model] = scores
 
     print(model_info)
@@ -25,7 +62,7 @@ def gen_info(model):
 
     return model_info
 
-models = os.listdir("models")
+models = os.listdir("penalty_models")
 models.sort()
 info = list(map(gen_info, models))
 
